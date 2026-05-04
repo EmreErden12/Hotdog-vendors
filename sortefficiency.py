2e030ba55ec5a3d3767b2bf5d7f91aebe189a739
EmreErden12 <6903@winchmore.enfield.sch.uk>
2026-05-04 20:04:07 +0100

    Add files via upload

---
+++ sortefficiency.py
@@ -0,0 +1,46 @@
+import time
+import copy
+
+#  number of runs for average timing 
+runs = 100
+
+print("\n--- SORT EFFICIENCY TEST ---")
+
+
+# 1. BUBBLE SORT TIMING
+
+start_time = time.perf_counter()
+
+for i in range(runs):
+
+    # copy data so each run starts fresh
+    temp_data = copy.deepcopy(hotdog_data)
+
+    bubble_sort_by_brand(temp_data)
+
+end_time = time.perf_counter()
+
+bubble_time = (end_time - start_time) / runs
+
+
+# 2. QUICK SORT TIMING
+
+start_time = time.perf_counter()
+
+for i in range(runs):
+
+    # copy data so each run starts fresh
+    temp_data = copy.deepcopy(hotdog_data)
+
+    quick_sort(temp_data)
+
+end_time = time.perf_counter()
+
+quick_time = (end_time - start_time) / runs
+
+
+# 3. RESULTS OUTPUT
+
+print("\nSORTING EFFICIENCY RESULTS")
+print("Bubble Sort Time:", bubble_time)
+print("Quick Sort Time:", quick_time)
